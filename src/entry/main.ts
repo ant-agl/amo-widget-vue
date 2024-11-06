62932005474002af53bd196210c72d42481c2672
ant-agl <aglanton31@gmail.com>
2024-11-06 19:59:42 +0300

    Up args name

--- src/entry/main.ts
+++ src/entry/main.ts
@@ -3,8 +3,8 @@ import type { DefineComponent } from "vue";
 import App from "@/views/App.vue";
 import { initApp } from "@/initApp";
 
-export default function (self: WidgetAmo) {
-  const app = initApp(App as DefineComponent, self);
+export default function (widget: WidgetAmo) {
+  const app = initApp(App as DefineComponent, widget);
   app.mount(".card-widgets__elements"); // в реальной разработке нужно использовать id
 
   return app;
